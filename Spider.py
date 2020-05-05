496a173fb07be5f4e12d827ced37dd3d66390771
KpLi0rn <563164617@qq.com>
2020-05-05 12:15:34 +0800

    update

--- Spider.py
+++ Spider.py
@@ -31,44 +31,47 @@ def cmd():
     parser.add_option('-r',dest='source',help='you txt path')
     # parser.add_option('-c', '--cookie', dest='cookie', help='write your cookie')
     (options, args) = parser.parse_args()
-    # if options.query is None or options.cookie is None:
-    # if options.query is None:
-    #     parser.print_help()
-    #     sys.exit(0)
-    # else:
     return options,args
 
 class FofaSpider(object):
 
     # query 就是我们的查询语句
-    def __init__(self,page,Cookie,query):
+    def __init__(self,Cookie,query):
         self.q = quote(query)
         self.qbase64 = quote(str(base64.b64encode(query.encode()),encoding='utf-8'))
-        self.page = page
+        # self.page = page
         self.s = requests.Session()
         self.ua = UserAgent()
         # 这里的cookie 后面改成input 用户自己输入
         self.header = {"User-Agent": self.ua.random,"Cookie": Cookie}
         self.domains = set()
+        self.page = 1
 
-    def spider(self,page):
+    def spider(self):
         try:
-            target = 'https://fofa.so/result?page={}&q={}&qbase64={}'.format(page,self.q, self.qbase64)
-            res = self.s.get(url=target, headers=self.header).text
-            selector = etree.HTML(res)
-            domain = selector.xpath('//*[@id="ajax_content"]/div/div/div/a/text()')
-            for value in domain:
-                value.strip(' ')
-                self.domains.add(value)
-                print(value)
-            time.sleep(random.randint(7,10))
+            while(self.page):
+                target = 'https://fofa.so/result?page={}&q={}&qbase64={}'.format(self.page,self.q, self.qbase64)
+                res = self.s.get(url=target, headers=self.header).text
+                # time.sleep(random.randint(7,10))
+                selector = etree.HTML(res)
+                domain = selector.xpath('//*[@id="ajax_content"]/div/div/div/a/text()')
+                if len(domain) == 0:
+                    print("爬取结束,或您的账号已无法再爬取")
+                    break
+                print("\033[31m第%s页\033[0m" % str(self.page))
+                for value in domain:
+                    value.strip(' ')
+                    self.domains.add(value)
+                    print(value)
+                self.page+=1
+                    # sys.exit(0)
+                time.sleep(random.randint(3,7))
+
         except Exception as e:
             print(e)
 
     def run(self):
-        pool = ThreadPoolExecutor(2)
-        [pool.submit(self.spider,i) for i in range(1,self.page)]
-
+        self.spider()
 
 if __name__ == '__main__':
     banner()
@@ -80,10 +83,10 @@ if __name__ == '__main__':
             # cookie = "".join(args)
             for value in file.readlines():
                 value = value.strip('\n')
-                spider = FofaSpider(options.page + 1, cookie, value)
+                spider = FofaSpider( cookie, value)
                 spider.run()
     else:
     # cookie = "".join(args)
-        spider = FofaSpider(options.page+1,cookie,options.query)
+        spider = FofaSpider(cookie,options.query)
         spider.run()
 
