cf85538fc1442f29d703c09fdb9eec8b3f7952a6
KpLi0rn <563164617@qq.com>
2020-11-30 13:33:01 +0800

    update

--- FofaSpider.py
+++ FofaSpider.py
@@ -1,6 +1,7 @@
 # !/usr/bin/env python
 # coding: utf-8
 
+import os
 import sys
 import re
 import time
@@ -29,11 +30,16 @@ def banner():
 
 
 def cmd():
+    if os.name == "nt":
+        print("这是windows")
+
     parser = optparse.OptionParser()
     parser.add_option('-q', '--query', dest='query', type=str, help='write the query you want')
     parser.add_option('-r',dest='source',help='you txt path')   # 批量搜索文件
     parser.add_option('-p',dest='startpage',default=1,type=int,help='input the StartPage')
     parser.add_option('-s',dest='spidernum',default=0,type=int,help='intput the Spider number')
+
+
     (options, args) = parser.parse_args()
     return options,args
 
@@ -41,11 +47,12 @@ class FofaSpider(object):
 
     # query 就是我们的查询语句
     def __init__(self,Cookie,query,startpage,spidernum):
-        self.q = quote(query)
+        self.query = "".join([query.strip('\'') if os.name == 'nt' else query])
+        self.q = quote(query)   # 将传入的数据url编码
         self.qbase64 = quote(str(base64.b64encode(query.encode()),encoding='utf-8'))
         self.UserAgent = ["Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16","Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36","Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50","Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0","Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11","Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)"]
         # 这里的cookie 后面改成input 用户自己输入
-        self.Cookie = Cookie
+        self.Cookie = "".join([Cookie.strip('\'') if os.name == 'nt' else Cookie])
         self.page = 1
         self.startpage = startpage
         self.spidernum = spidernum
@@ -53,6 +60,7 @@ class FofaSpider(object):
         self.name = ("".join((self.dt.split(" ")[0]).split('-'))) + "_" + "".join((self.dt.split(" ")[1]).split(':'))
 
     def spider(self):
+
         global i
         i=0
         compile = re.compile('HTTP/1.1 (\d+) ')
@@ -84,6 +92,7 @@ class FofaSpider(object):
                 if "error 500程序出错了" in res:
                     sys.stdout.write("\033[31m500程序出错了,请检查查询语句是否正确\n\033[0m")
                     sys.exit(0)
+
                 if "游客使用高级语法只能显示第一页" in res:
                     sys.stdout.write('\033[31m游客使用高级语法只能显示第一页\n\033[0m')
                     sys.stdout.write(
