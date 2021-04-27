f33eecf31d65d99b0c7e84676a1a5bd6bac4ee01
KpLi0rn <563164617@qq.com>
2021-04-27 15:18:39 +0800

    verify fix

--- FofaSpider.py
+++ FofaSpider.py
@@ -39,12 +39,12 @@ def Cmd():
 
 class FofaSpider(object):
 
-    def __init__(self,Token,Query,StartPage,SpiderNum):
+    def __init__(self,Cookie,Query,StartPage,SpiderNum):
         self.q = quote(Query)
         self.qbase64 = quote(str(base64.b64encode(Query.encode()),encoding='utf-8'))
         self.UserAgent = ["Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16","Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36","Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50","Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0","Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11","Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)"]
         # 这里的cookie 后面改成input 用户自己输入
-        self.token = Token
+        self.cookie = Cookie
         self.page = 1
         self.startpage = StartPage
         self.spidernum = SpiderNum
@@ -60,7 +60,7 @@ class FofaSpider(object):
         sheet = filename.add_sheet('result')
         header = {
             "User-Agent": random.choice(self.UserAgent),
-            "Authorization": self.token
+            "cookie": self.cookie
         }
         url = 'https://fofa.so/result?q={}&qbase64={}&full=true'.format(self.q, self.qbase64)
         html = requests.get(url=url, headers=header).text
@@ -147,7 +147,7 @@ class FofaSpider(object):
 if __name__ == '__main__':
     banner()
     options, args = Cmd()
-    token = "".join(args)
+    cookie = "".join(args)
 
     try:
         if options.source is not None:
@@ -155,12 +155,12 @@ if __name__ == '__main__':
                 for value in file.readlines():
                     value = value.strip('\n')
                     try:
-                        spider = FofaSpider(token, value, options.startpage, options.spidernum)
+                        spider = FofaSpider(cookie, value, options.startpage, options.spidernum)
                         spider.run()
                     except:
                         pass
         else:
-            spider = FofaSpider(token,options.query, options.startpage, options.spidernum)
+            spider = FofaSpider(cookie,options.query, options.startpage, options.spidernum)
             spider.run()
 
     except KeyboardInterrupt:
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 目前处于代码初期后期会继续更新，欢迎师傅们加QQ提意见：NTYzMTY0NjE3 (防麦片)
 
-
 # FofaSpider
-由于 Fofa 该用 jwt token 校验登录所以对应的也改为token
 
 本脚本基于python3开发，支持批量爬取功能，将语句写在txt中会进行批量查询
 
@@ -30,24 +28,20 @@ pip install -r requirements.txt
 
 # 使用说明
 
-# Token
 
-登录之后在请求包的 header 中，Authorization 的值就是我们的Token
-Authorization: xxxxxxxxx.xxxxxxx.xxxxxxxxxx
-`python3 Spider.py 'xxxxx.xxxxx.xxxxxx' -q 'domain="baidu.com"||title="百度"' `
 
 ## Linux
 
-`python3 Spider.py 'Token' -q 'domain="baidu.com"||title="百度"' `
+`python3 Spider.py 'Cookie' -q 'domain="baidu.com"||title="百度"' `
 
 -p 参数可自定义从第几页开始爬取，可不加不加 -p 参数则默认为1 -s 爬取几页(该参数可不加)
 
-`python3 Spider.py 'Token' -q 'domain="baidu.com"' -p 5 -s 10`
+`python3 Spider.py 'Cookie' -q 'domain="baidu.com"' -p 5 -s 10`
 
 ## Windows
 
 windows下强烈推荐在powershell中运行
-`python3 Spider.py 'Token' -q 'domain="baidu.com"||title="百度"' `
+`python3 Spider.py 'Cookie' -q 'domain="baidu.com"||title="百度"' `
 
 
 ## 支持批量查询语句
@@ -58,34 +52,10 @@ domain="baidu.com"
 domain="bilibili.com"
 
 **Linux下**
-`python3 Spider.py -r 你文本的路径 'Token'`
+`python3 Spider.py -r 你文本的路径 'Cookie'`
 
 **Windows下**
-`python3 Spider.py -r 你文本的路径 Token`
+`python3 Spider.py -r 你文本的路径 Cookie`
 
-## 更新日志：
-05.13 新增输出结果到文本功能,取消了fakeUseragent的使用
 
-05.14 新增部分代码高亮,以及结果判断
-
-05.18 修改了输出文件名的格式，取消了md5对文件名加密命名方式，将文件名改成时间方式，
-      删去了部分多余结构，爬取过程中用户中断也会将之前爬取的信息写入txt中
-      
-05.20 修改了部分逻辑添加报错文本机制
-
-05.21 修改了部分小bug
-
-05.22 大改，使用了xlwt库需要进行依赖安装，输出中会显示状态码，输出结果改成excel表格(csv格式)，在爬取过程中会同时爬取fofa的状态码，test.py为原来的代码,修改了爬取不全的bug,循环改成for循环
-
-05.25 增加了一个判断，修改了部分协议类数据爬不到的情况
-
-05.26 增加了改进版，改进版中不会漏爬，但是取消了状态码的爬取
-
-05.30 增加了 -p 参数 自定义从第几页开始爬 不加则默认为1
-
-07.04 增加功能爬取几页
-
-07.14 适配fofa新版本
-
-11.29 小问题修改，添加txt输出
 
